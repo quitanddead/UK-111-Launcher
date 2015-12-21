8aa8cc93e0edfcd960d23415c9152d854ba53d30
DaedalusOS <ftbkingsmc@gmail.com>
2015-12-21 14:54:43 -0600

    Added all launch parameters

--- UK_111_Launcher/Form1.Designer.cs
+++ UK_111_Launcher/Form1.Designer.cs
@@ -29,22 +29,44 @@
         private void InitializeComponent()
         {
             this.nsTheme1 = new NSTheme();
+            this.nsControlButton1 = new NSControlButton();
             this.nsTabControl1 = new NSTabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.nsTabControl2 = new NSTabControl();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.tabPage6 = new System.Windows.Forms.TabPage();
-            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.nsButton2 = new NSButton();
+            this.nsButton1 = new NSButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.nsButton6 = new NSButton();
+            this.nsButton3 = new NSButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.nsButton7 = new NSButton();
+            this.nsButton4 = new NSButton();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.nsButton8 = new NSButton();
+            this.nsButton5 = new NSButton();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.nsTheme1.SuspendLayout();
             this.nsTabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.nsTabControl2.SuspendLayout();
             this.tabPage4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.tabPage7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // nsTheme1
@@ -53,6 +75,7 @@
             this.nsTheme1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.nsTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.nsTheme1.Colors = new Bloom[0];
+            this.nsTheme1.Controls.Add(this.nsControlButton1);
             this.nsTheme1.Controls.Add(this.nsTabControl1);
             this.nsTheme1.Customization = "";
             this.nsTheme1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -70,6 +93,19 @@
             this.nsTheme1.Text = "UK 111 Launcher";
             this.nsTheme1.TransparencyKey = System.Drawing.Color.Empty;
             this.nsTheme1.Transparent = false;
+            // 
+            // nsControlButton1
+            // 
+            this.nsControlButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nsControlButton1.ControlButton = NSControlButton.Button.Close;
+            this.nsControlButton1.Location = new System.Drawing.Point(1377, 4);
+            this.nsControlButton1.Margin = new System.Windows.Forms.Padding(0);
+            this.nsControlButton1.MaximumSize = new System.Drawing.Size(18, 20);
+            this.nsControlButton1.MinimumSize = new System.Drawing.Size(18, 20);
+            this.nsControlButton1.Name = "nsControlButton1";
+            this.nsControlButton1.Size = new System.Drawing.Size(18, 20);
+            this.nsControlButton1.TabIndex = 1;
+            this.nsControlButton1.Text = "nsControlButton1";
             // 
             // nsTabControl1
             // 
@@ -98,25 +134,6 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Servers";
             // 
-            // tabPage2
-            // 
-            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.tabPage2.Location = new System.Drawing.Point(119, 4);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1270, 664);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Staff";
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.tabPage3.Location = new System.Drawing.Point(119, 4);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1270, 664);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Mods";
-            // 
             // nsTabControl2
             // 
             this.nsTabControl2.Alignment = System.Windows.Forms.TabAlignment.Left;
@@ -137,6 +154,8 @@
             // tabPage4
             // 
             this.tabPage4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage4.Controls.Add(this.nsButton2);
+            this.tabPage4.Controls.Add(this.nsButton1);
             this.tabPage4.Controls.Add(this.pictureBox1);
             this.tabPage4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.tabPage4.Location = new System.Drawing.Point(119, 4);
@@ -146,33 +165,23 @@
             this.tabPage4.TabIndex = 0;
             this.tabPage4.Text = "Chernarus";
             // 
-            // tabPage5
+            // nsButton2
             // 
-            this.tabPage5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.tabPage5.Location = new System.Drawing.Point(119, 4);
-            this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(1135, 644);
-            this.tabPage5.TabIndex = 1;
-            this.tabPage5.Text = "Taviana";
+            this.nsButton2.Location = new System.Drawing.Point(911, 18);
+            this.nsButton2.Name = "nsButton2";
+            this.nsButton2.Size = new System.Drawing.Size(218, 32);
+            this.nsButton2.TabIndex = 2;
+            this.nsButton2.Text = "Copy IP to Clipboard";
+            this.nsButton2.Click += new System.EventHandler(this.nsButton2_Click);
             // 
-            // tabPage6
+            // nsButton1
             // 
-            this.tabPage6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.tabPage6.Location = new System.Drawing.Point(119, 4);
-            this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(1135, 644);
-            this.tabPage6.TabIndex = 2;
-            this.tabPage6.Text = "Napf";
-            // 
-            // tabPage7
-            // 
-            this.tabPage7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.tabPage7.Location = new System.Drawing.Point(119, 4);
-            this.tabPage7.Name = "tabPage7";
-            this.tabPage7.Size = new System.Drawing.Size(1135, 644);
-            this.tabPage7.TabIndex = 3;
-            this.tabPage7.Text = "Exile";
+            this.nsButton1.Location = new System.Drawing.Point(6, 18);
+            this.nsButton1.Name = "nsButton1";
+            this.nsButton1.Size = new System.Drawing.Size(218, 32);
+            this.nsButton1.TabIndex = 1;
+            this.nsButton1.Text = "Join Server";
+            this.nsButton1.Click += new System.EventHandler(this.nsButton1_Click);
             // 
             // pictureBox1
             // 
@@ -183,6 +192,164 @@
             this.pictureBox1.Size = new System.Drawing.Size(1135, 644);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage5.Controls.Add(this.nsButton6);
+            this.tabPage5.Controls.Add(this.nsButton3);
+            this.tabPage5.Controls.Add(this.pictureBox2);
+            this.tabPage5.Location = new System.Drawing.Point(119, 4);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1135, 644);
+            this.tabPage5.TabIndex = 1;
+            this.tabPage5.Text = "Taviana";
+            // 
+            // nsButton6
+            // 
+            this.nsButton6.Location = new System.Drawing.Point(911, 18);
+            this.nsButton6.Name = "nsButton6";
+            this.nsButton6.Size = new System.Drawing.Size(218, 32);
+            this.nsButton6.TabIndex = 2;
+            this.nsButton6.Text = "Copy IP to Clipboard";
+            this.nsButton6.Click += new System.EventHandler(this.nsButton6_Click);
+            // 
+            // nsButton3
+            // 
+            this.nsButton3.Location = new System.Drawing.Point(6, 18);
+            this.nsButton3.Name = "nsButton3";
+            this.nsButton3.Size = new System.Drawing.Size(218, 32);
+            this.nsButton3.TabIndex = 1;
+            this.nsButton3.Text = "Join Server";
+            this.nsButton3.Click += new System.EventHandler(this.nsButton3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::UK_111_Launcher.Properties.Resources.new_taviana_background;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1135, 644);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage6.Controls.Add(this.nsButton7);
+            this.tabPage6.Controls.Add(this.nsButton4);
+            this.tabPage6.Controls.Add(this.pictureBox3);
+            this.tabPage6.Location = new System.Drawing.Point(119, 4);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(1135, 644);
+            this.tabPage6.TabIndex = 2;
+            this.tabPage6.Text = "Napf";
+            // 
+            // nsButton7
+            // 
+            this.nsButton7.Location = new System.Drawing.Point(911, 18);
+            this.nsButton7.Name = "nsButton7";
+            this.nsButton7.Size = new System.Drawing.Size(218, 32);
+            this.nsButton7.TabIndex = 2;
+            this.nsButton7.Text = "Copy IP to Clipboard";
+            this.nsButton7.Click += new System.EventHandler(this.nsButton7_Click);
+            // 
+            // nsButton4
+            // 
+            this.nsButton4.Location = new System.Drawing.Point(6, 18);
+            this.nsButton4.Name = "nsButton4";
+            this.nsButton4.Size = new System.Drawing.Size(218, 32);
+            this.nsButton4.TabIndex = 1;
+            this.nsButton4.Text = "Join Server";
+            this.nsButton4.Click += new System.EventHandler(this.nsButton4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::UK_111_Launcher.Properties.Resources.new_napf_background;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(1135, 644);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage7.Controls.Add(this.nsButton8);
+            this.tabPage7.Controls.Add(this.nsButton5);
+            this.tabPage7.Controls.Add(this.pictureBox6);
+            this.tabPage7.Controls.Add(this.pictureBox5);
+            this.tabPage7.Controls.Add(this.pictureBox4);
+            this.tabPage7.Location = new System.Drawing.Point(119, 4);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(1135, 644);
+            this.tabPage7.TabIndex = 3;
+            this.tabPage7.Text = "Exile";
+            // 
+            // nsButton8
+            // 
+            this.nsButton8.Location = new System.Drawing.Point(911, 18);
+            this.nsButton8.Name = "nsButton8";
+            this.nsButton8.Size = new System.Drawing.Size(218, 32);
+            this.nsButton8.TabIndex = 4;
+            this.nsButton8.Text = "Copy IP to Clipboard";
+            this.nsButton8.Click += new System.EventHandler(this.nsButton8_Click);
+            // 
+            // nsButton5
+            // 
+            this.nsButton5.Location = new System.Drawing.Point(6, 18);
+            this.nsButton5.Name = "nsButton5";
+            this.nsButton5.Size = new System.Drawing.Size(218, 32);
+            this.nsButton5.TabIndex = 3;
+            this.nsButton5.Text = "Join Server";
+            this.nsButton5.Click += new System.EventHandler(this.nsButton5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = global::UK_111_Launcher.Properties.Resources.new_exile_background;
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(1135, 644);
+            this.pictureBox6.TabIndex = 2;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(1135, 644);
+            this.pictureBox5.TabIndex = 1;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(1135, 644);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage2.Location = new System.Drawing.Point(119, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1270, 664);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Staff";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage3.Location = new System.Drawing.Point(119, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1270, 664);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Mods";
             // 
             // Form1
             // 
@@ -199,6 +366,14 @@
             this.nsTabControl2.ResumeLayout(false);
             this.tabPage4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tabPage6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.tabPage7.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -216,6 +391,20 @@
         private System.Windows.Forms.TabPage tabPage6;
         private System.Windows.Forms.TabPage tabPage7;
         private System.Windows.Forms.TabPage tabPage3;
+        private NSButton nsButton1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private NSButton nsButton2;
+        private NSButton nsButton3;
+        private NSButton nsButton4;
+        private NSButton nsButton5;
+        private NSButton nsButton6;
+        private NSButton nsButton7;
+        private NSButton nsButton8;
+        private NSControlButton nsControlButton1;
     }
 }
 
--- UK_111_Launcher/Form1.cs
+++ UK_111_Launcher/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,94 @@ namespace UK_111_Launcher
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Chernarus
+        private void nsButton1_Click(object sender, EventArgs e)
+        {
+            RegistryKey regKey = Registry.CurrentUser;
+            regKey = regKey.OpenSubKey(@"Software\Valve\Steam");
+
+            if (regKey != null)
+            {
+                string installpath = regKey.GetValue("SteamPath").ToString();
+                Console.WriteLine("Steam Directory: " + installpath);
+                Console.WriteLine("Launching Game!");
+
+                System.Diagnostics.Process.Start(installpath + "\\steamapps\\common\\Arma 2 Operation Arrowhead\\arma2oa.exe", "-mod=@DayZ_Epoch;@DayZOverwatch; -skipintro -noSplash -connect=151.80.33.151 -port=2302");
+            }
+        }
+
+        // Taviana
+        private void nsButton3_Click(object sender, EventArgs e)
+        {
+            RegistryKey regKey = Registry.CurrentUser;
+            regKey = regKey.OpenSubKey(@"Software\Valve\Steam");
+
+            if (regKey != null)
+            {
+                string installpath = regKey.GetValue("SteamPath").ToString();
+                Console.WriteLine("Steam Directory: " + installpath);
+                Console.WriteLine("Launching Game!");
+
+                System.Diagnostics.Process.Start(installpath + "\\steamapps\\common\\Arma 2 Operation Arrowhead\\arma2oa.exe", "-mod=@DayZ_Epoch;@DayZOverwatch;@DayZOrigins; -skipintro -noSplash -connect=151.80.33.151 -port=4302");
+            }
+        }
+
+        // Napf
+        private void nsButton4_Click(object sender, EventArgs e)
+        {
+            RegistryKey regKey = Registry.CurrentUser;
+            regKey = regKey.OpenSubKey(@"Software\Valve\Steam");
+
+            if (regKey != null)
+            {
+                string installpath = regKey.GetValue("SteamPath").ToString();
+                Console.WriteLine("Steam Directory: " + installpath);
+                Console.WriteLine("Launching Game!");
+
+                System.Diagnostics.Process.Start(installpath + "\\steamapps\\common\\Arma 2 Operation Arrowhead\\arma2oa.exe", "-mod=@DayZ_Epoch;@DayZOverwatch; -skipintro -noSplash -connect=151.80.33.151 -port=3302");
+            }
+        }
+
+        // Exile
+        private void nsButton5_Click(object sender, EventArgs e)
+        {
+            RegistryKey regKey = Registry.CurrentUser;
+            regKey = regKey.OpenSubKey(@"Software\Valve\Steam");
+
+            if (regKey != null)
+            {
+                string installpath = regKey.GetValue("SteamPath").ToString();
+                Console.WriteLine("Steam Directory: " + installpath);
+                Console.WriteLine("Launching Game!");
+
+                System.Diagnostics.Process.Start(installpath + "\\steamapps\\common\\Arma 3\\arma3launcher.exe", "-noLauncher -useBE -mod=@Exile -skipintro -noSplash -connect=151.80.33.151 -port=5302");
+            }
+        }
+
+        // Chernarus IP
+        private void nsButton2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("151.80.33.151:2302");
+        }
+
+        // Taviana IP
+        private void nsButton6_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("151.80.33.151:4302");
+        }
+
+        // Napf IP
+        private void nsButton7_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("151.80.33.151:3302");
+        }
+
+        // Exile IP
+        private void nsButton8_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("94.23.0.19:2302");
         }
     }
 }
