e71b02c2fec1224c8364b1881fb19afba5ffcd18
DaedalusOS <ftbkingsmc@gmail.com>
2015-12-21 14:31:50 -0600

    First Commit

---
+++ UK_111_Launcher/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace UK_111_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nsTheme1 = new NSTheme();
+            this.nsTabControl1 = new NSTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.nsTabControl2 = new NSTabControl();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.nsTheme1.SuspendLayout();
+            this.nsTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.nsTabControl2.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nsTheme1
+            // 
+            this.nsTheme1.AccentOffset = 42;
+            this.nsTheme1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.nsTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.nsTheme1.Colors = new Bloom[0];
+            this.nsTheme1.Controls.Add(this.nsTabControl1);
+            this.nsTheme1.Customization = "";
+            this.nsTheme1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nsTheme1.Font = new System.Drawing.Font("Verdana", 8F);
+            this.nsTheme1.Image = null;
+            this.nsTheme1.Location = new System.Drawing.Point(0, 0);
+            this.nsTheme1.Movable = true;
+            this.nsTheme1.Name = "nsTheme1";
+            this.nsTheme1.NoRounding = false;
+            this.nsTheme1.Sizable = true;
+            this.nsTheme1.Size = new System.Drawing.Size(1399, 707);
+            this.nsTheme1.SmartBounds = true;
+            this.nsTheme1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.nsTheme1.TabIndex = 0;
+            this.nsTheme1.Text = "UK 111 Launcher";
+            this.nsTheme1.TransparencyKey = System.Drawing.Color.Empty;
+            this.nsTheme1.Transparent = false;
+            // 
+            // nsTabControl1
+            // 
+            this.nsTabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.nsTabControl1.Controls.Add(this.tabPage1);
+            this.nsTabControl1.Controls.Add(this.tabPage2);
+            this.nsTabControl1.Controls.Add(this.tabPage3);
+            this.nsTabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.nsTabControl1.ItemSize = new System.Drawing.Size(28, 115);
+            this.nsTabControl1.Location = new System.Drawing.Point(3, 32);
+            this.nsTabControl1.Multiline = true;
+            this.nsTabControl1.Name = "nsTabControl1";
+            this.nsTabControl1.SelectedIndex = 0;
+            this.nsTabControl1.Size = new System.Drawing.Size(1393, 672);
+            this.nsTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.nsTabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage1.Controls.Add(this.nsTabControl2);
+            this.tabPage1.Location = new System.Drawing.Point(119, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1270, 664);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Servers";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage2.Location = new System.Drawing.Point(119, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1270, 664);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Staff";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage3.Location = new System.Drawing.Point(119, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1270, 664);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Mods";
+            // 
+            // nsTabControl2
+            // 
+            this.nsTabControl2.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.nsTabControl2.Controls.Add(this.tabPage4);
+            this.nsTabControl2.Controls.Add(this.tabPage5);
+            this.nsTabControl2.Controls.Add(this.tabPage6);
+            this.nsTabControl2.Controls.Add(this.tabPage7);
+            this.nsTabControl2.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.nsTabControl2.ItemSize = new System.Drawing.Size(28, 115);
+            this.nsTabControl2.Location = new System.Drawing.Point(6, 6);
+            this.nsTabControl2.Multiline = true;
+            this.nsTabControl2.Name = "nsTabControl2";
+            this.nsTabControl2.SelectedIndex = 0;
+            this.nsTabControl2.Size = new System.Drawing.Size(1258, 652);
+            this.nsTabControl2.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.nsTabControl2.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage4.Controls.Add(this.pictureBox1);
+            this.tabPage4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tabPage4.Location = new System.Drawing.Point(119, 4);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1135, 644);
+            this.tabPage4.TabIndex = 0;
+            this.tabPage4.Text = "Chernarus";
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage5.Location = new System.Drawing.Point(119, 4);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1135, 644);
+            this.tabPage5.TabIndex = 1;
+            this.tabPage5.Text = "Taviana";
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage6.Location = new System.Drawing.Point(119, 4);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(1135, 644);
+            this.tabPage6.TabIndex = 2;
+            this.tabPage6.Text = "Napf";
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.tabPage7.Location = new System.Drawing.Point(119, 4);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(1135, 644);
+            this.tabPage7.TabIndex = 3;
+            this.tabPage7.Text = "Exile";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::UK_111_Launcher.Properties.Resources.new_chernarus_background;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1135, 644);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1399, 707);
+            this.Controls.Add(this.nsTheme1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.nsTheme1.ResumeLayout(false);
+            this.nsTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.nsTabControl2.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NSTheme nsTheme1;
+        private NSTabControl nsTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private NSTabControl nsTabControl2;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
